9677367aac96e1a198f981c7a5a401320dc71953
DmytroZaburannyi <zaburanniy2011@gmail.com>
2023-02-02 11:38:06 +0200

    commit 1.0

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import {
+  searchCocktailByName,
+  searchCocktailByFirstLetter,
+  searchCocktailByIngredient,
+  searchCocktailById,
+  searchCocktailRandom,
+  searchIngredientsByName,
+  searchIngredientsById,
+} from './js/ApiServise';
+import addFavorite from './js/addFavorite';
+import changeTheme from './js/changeTheme';
+import heroDropdown from './js/hero-dropdown';
+import heroSearchMobile from './js/hero-search-mobile';
+import heroSearch from './js/hero-search';
+import mobileMenu from './js/mobileMenu';
+// import modalCloseOpen from './js/modal-close-open';
+// import modalFetchCocktail from './js/modalFetchCocktail';
+// import modalFetchIngridient from './js/modalFetchIngridient';
+import renderCocktails from './js/renderCocktails';
+import renderFavoriteCock from './js/renderFavoriteCock';
+import searchByFirstLetter from './js/search-by-first-letter';
+import searchByFirstLetter from './js/search-header';
+
+import { openModal, getIngridient } from './js/modalFetchCocktail';
+import { openIngredientModal } from './js/modalFetchIngridient';
